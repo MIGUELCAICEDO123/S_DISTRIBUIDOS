1e38453f8af33c0cec669c04090b3c866c6ad046
MIGUELCAICEDO123 <miguelcaicedo64@gmail.com>
2022-11-21 23:32:38 -0500

    4 intento

--- registry-eureka/src/main/java/itp/instituto/registry/RegistryEurekaApplication.java
+++ registry-eureka/src/main/java/itp/instituto/registry/RegistryEurekaApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistryEurekaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RegistryEurekaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryEurekaApplication.class, args);
+    }
 }
